db407a7dc62b04992e5da0cd4d813dfc3e39f1d9
Josh Liben <josh@topology.health>
2024-04-22 16:04:45 -0400

    updated to try test request

--- index.ts
+++ index.ts
@@ -1,50 +1,63 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import axios from 'axios';
 import * as AWS from 'aws-sdk';
 
 // Initialize the Lambda client
-const lambda = new AWS.Lambda();
+// const lambda = new AWS.Lambda();
 
 // Define the handler function
-export const handler: APIGatewayProxyHandler = async (event) => {
-    try {
-        // Step 1: Invoke another Lambda function using IAM role
-        const lambdaResponse = await lambda.invoke({
-            FunctionName: 'your-other-lambda-function-name',
-            InvocationType: 'RequestResponse',
-            Payload: JSON.stringify({ /* your payload here */ })
-        }).promise();
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const eventBody = event.body
+    if (!eventBody) throw new Error('Body must contain data')
+    const eventBodyJson = JSON.parse(eventBody) as EventJson
+    //TODO: Step 1: Invoke another Lambda function using IAM role
+    // const lambdaResponse = await lambda.invoke({
+    //   FunctionName: 'your-other-lambda-function-name',
+    //   InvocationType: 'RequestResponse'
+    // }).promise();
 
-        // Assuming the Lambda function returns the token directly
-        const token = JSON.parse(lambdaResponse.Payload as string).token;
+    const tokenLambdaFunctionResponse = await axios.get('https://wrzjvsr99b.execute-api.ca-central-1.amazonaws.com/sandbox/insighthealth-backend-service')
 
-        // Step 2: Make an authenticated HTTP request to an API Gateway endpoint
-        const apiResponse = await axios.get('https://<api-id>.execute-api.<region>.amazonaws.com/<stage>/<resource>', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+    // Assuming the Lambda function returns the token directly
+    const token = JSON.parse(tokenLambdaFunctionResponse.data as string).tokenResponse.access_token;
+    const path = eventBodyJson.path
+    const queryParams = eventBodyJson.queryParameters
 
-        // Step 3: Log the successful execution and return data
-        console.log('API Response:', apiResponse.data);
+    // Step 2: Make an authenticated HTTP request to an API Gateway endpoint
+    const apiResponse = await axios.get(`https://kwfklwibs0.execute-api.ca-central-1.amazonaws.com/${path}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      ...(queryParams ? {params: queryParams} : {})
+    });
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Request successful',
-                apiData: apiResponse.data
-            }),
-        };
-    } catch (error) {
-        console.error('Error handling the request:', error);
+    // Step 3: Log the successful execution and return data
+    console.log('API Response:', apiResponse.data);
 
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'Error handling the request',
-                errorMessage: error.message,
-                errorDetails: error.response?.data || 'No additional error information',
-            }),
-        };
-    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Request successful',
+        apiData: apiResponse.data,
+        apiHeaders: apiResponse.headers
+      }),
+    };
+  } catch (err) {
+    const error = err as Error
+    console.error('Error handling the request:', error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Error handling the request',
+        errorMessage: error.message
+      }),
+    };
+  }
 };
+
+type EventJson = {
+  path: string
+  queryParameters?: any
+}
