ca3883aeb61f030939a8d9b683295b3d70c232f2
Josh Liben <josh@topology.health>
2024-04-23 17:13:22 -0400

    stateless fhri proxy request execution

--- index.ts
+++ index.ts
@@ -2,40 +2,38 @@ import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 import { APIGatewayEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import axios from 'axios';
 
-// Initialize the Lambda client
 
-// Define the handler function
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   try {
     const eventBody = event.body
     if (!eventBody) throw new Error('Body must contain data')
     const eventBodyJson = eventBody as unknown as EventJson
+    const tokenFunctionName = eventBodyJson.token_function_name
 
-    //Step 1: Invoke another Lambda function using IAM role
     const lambda = new LambdaClient({});
     const tokenLambdaFunctionResponse = await lambda.send(new InvokeCommand({
-      FunctionName: 'backend-service-dev',
+      FunctionName: tokenFunctionName,
       InvocationType: 'RequestResponse'
     }));
 
-    // const tokenLambdaFunctionResponse = await axios.get('https://d5nfsx63n2.execute-api.ca-central-1.amazonaws.com/sandbox/mindtrace')
     const { Payload, LogResult } = tokenLambdaFunctionResponse
     if (!Payload) throw new Error('Payload must be defined.')
     const payloadAsJson = JSON.parse(Buffer.from(Payload).toString());
-    // Assuming the Lambda function returns the token directly
+
     const token = JSON.parse(payloadAsJson.body).tokenResponse.access_token;
     const path = eventBodyJson.path
+    const host = eventBodyJson.host
     const queryParams = eventBodyJson.queryParameters
 
-    // Step 2: Make an authenticated HTTP request to an API Gateway endpoint
-    const apiResponse = await axios.get(`https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/${path}`, {
+
+    const apiResponse = await axios.get(`${host}/${path}`, {
       headers: {
         Authorization: `Bearer ${token}`
       },
       ...(queryParams ? { params: queryParams } : {})
     });
 
-    // Step 3: Log the successful execution and return data
+
     console.log('API Response:', apiResponse.data);
 
     return {
@@ -61,6 +59,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent): P
 };
 
 type EventJson = {
-  path: string
+  path: string,
+  host: string,
+  token_function_name: string,
   queryParameters?: any
 }
